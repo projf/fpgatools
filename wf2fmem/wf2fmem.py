bdb7813e02cbf1fbe24e4fb2e2e4eafeb5f44478
Will Green <WillGreen@users.noreply.github.com>
2021-03-13 17:51:58 +0000

    Add z-coordinate to output.
    Add monkey model from Blender.

--- wf2fmem/wf2fmem.py
+++ wf2fmem/wf2fmem.py
@@ -24,37 +24,63 @@ base_name = os.path.splitext(input_file)[0]
 # print("input_file: {}".format(input_file))
 # print("base_name:  {}".format(base_name))
 
+## these parameters should be calculated automatically
+
+# cube
 SC = 220  # scale factor for model vertices
-# SC = 55  # scale factor for model vertices
-X_OFFS = 0.0  # horizontal offset
-# X_OFFS = 2.0  # horizontal offset
-Y_OFFS = 0.0  # vertical offset
+D_OFFS = 10   # draw offset (pixels to add to all dimensions)
+X_OFFS = 0.0  # X offset
+Y_OFFS = 0.0  # Y offset
+Z_OFFS = 0.0  # Z offset
+
+# # teapot
+# SC = 32  # scale factor for model vertices
+# D_OFFS = 10   # draw offset (pixels to add to all dimensions)
+# X_OFFS = 2.0  # X offset
+# Y_OFFS = 0.0  # Y offset
+# Z_OFFS = 3.434  # Z offset
+
+# # monkey
+# SC = 88  # scale factor for model vertices
+# D_OFFS = 10   # draw offset (pixels to add to all dimensions)
+# X_OFFS = 1.367188  # X offset
+# Y_OFFS = 0.984375  # Y offset
+# Z_OFFS = 0.851562  # Z offset
 
 verts = []
 faces = []
 
+def fmt_line(c0, c1):
+    if (c1 > c0):
+        return "{}{}".format(c0,c1)
+    else:
+        return "{}{}".format(c1,c0)
+
 def gen_lines(face):
     x_coords = []
     y_coords = []
+    z_coords = []
     for f in face:
         x_coords.append(verts[f-1][0])
         y_coords.append(verts[f-1][1])
+        z_coords.append(verts[f-1][2])
 
-    x_coords = [int(SC*(c+X_OFFS)) for c in x_coords]
-    y_coords = [int(SC*(c+Y_OFFS)) for c in y_coords]
+    x_coords = [int(SC*(c+X_OFFS)) + D_OFFS for c in x_coords]
+    y_coords = [int(SC*(c+Y_OFFS)) + D_OFFS for c in y_coords]
+    z_coords = [int(SC*(c+Z_OFFS)) + D_OFFS for c in z_coords]
 
-    hc = []
-    for x,y in zip(x_coords, y_coords):
-        hc.append("{:02X}{:02X}".format(x,y))
-        # hc.append("{:03}{:03}".format(x,y))
+    hc = []  # hex coordinates
+    for x,y,z in zip(x_coords, y_coords, z_coords):
+        hc.append("{:02X}{:02X}{:02X}".format(x,y,z))
 
-    print("{}{}".format(hc[0],hc[1]))
-    print("{}{}".format(hc[1],hc[2]))
+    # output coordinates for each line of the face
+    print(fmt_line(hc[0],hc[1]))
+    print(fmt_line(hc[1],hc[2]))
     if len(face) == 4:  # quad
-        print("{}{}".format(hc[2],hc[3]))
-        print("{}{}".format(hc[3],hc[0]))
+        print(fmt_line(hc[2],hc[3]))
+        print(fmt_line(hc[3],hc[0]))
     else:  # triangle
-        print("{}{}".format(hc[2],hc[0]))
+        print(fmt_line(hc[2],hc[0]))
 
 with open(input_file, 'r') as obj_f:
     for line in obj_f:
@@ -71,16 +97,18 @@ with open(input_file, 'r') as obj_f:
                     fv.append(int(v.partition('/')[0]))
                 faces.append(fv)
 
-print("There are {} vertices.".format(len(verts)))
-print("There are {} faces.".format(len(faces)))
-
 for f in faces:
     gen_lines(f)
+
+# print("There are {} vertices.".format(len(verts)))
+# print("There are {} faces.".format(len(faces)))
 
 # # Used to determine scale if we don't know it
 # max_x = max(verts, key=operator.itemgetter(0))
 # max_y = max(verts, key=operator.itemgetter(1))
+# max_z = max(verts, key=operator.itemgetter(2))
 # min_x = min(verts, key=operator.itemgetter(0))
 # min_y = min(verts, key=operator.itemgetter(1))
-# print("Max x={}, y={} ".format(max_x[0], max_y[1]))
-# print("Min x={}, y={} ".format(min_x[0], min_y[1]))
+# min_z = min(verts, key=operator.itemgetter(2))
+# print("Max x={}, y={}, z={} ".format(max_x[0], max_y[1], max_z[2]))
+# print("Min x={}, y={}, z={} ".format(min_x[0], min_y[1], min_z[2]))
